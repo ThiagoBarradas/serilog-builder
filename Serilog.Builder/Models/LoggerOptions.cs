c9acd5384635898b0aa59a48bafe49a24ec06ad8
Thaynara Santos <thaynaracsantos@gmail.com>
2019-04-01 17:25:44 -0300

    :heavy_plus_sign: Add logger options

---
+++ Serilog.Builder/Models/LoggerOptions.cs
@@ -0,0 +1,18 @@
+﻿namespace Serilog.Builder.Models
+{
+    /// <summary>
+    /// Logger options class
+    /// </summary>
+    public class LoggerOptions
+    {
+        /// <summary>
+        /// Application
+        /// </summary>
+        public string Application { get; set; }
+
+        /// <summary>
+        /// Domain
+        /// </summary>
+        public string Domain { get; set; }
+    }
+}
