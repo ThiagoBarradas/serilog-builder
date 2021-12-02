fd4fc3cb438b91c81e21b4b2d641902d32f8b051
Thiago Barradas <thiagobrowskt@gmail.com>
2021-12-02 00:10:39 -0300

    :bug: fixes bug in datadog sink

--- Serilog.Builder.Playground/Program.cs
+++ Serilog.Builder.Playground/Program.cs
@@ -9,7 +9,7 @@ namespace Serilog.Builder.Playground
         {
             LoggerBuilder builder = new LoggerBuilder();
             builder.UseSuggestedSetting("Domainnn", "Apppp");
-            builder.EnableDataDog("7edd7c7811f74f35519bc017b40ca572");
+            builder.EnableDataDog("xxx");
 
             var logger = builder.BuildConfiguration().CreateLogger();
 
